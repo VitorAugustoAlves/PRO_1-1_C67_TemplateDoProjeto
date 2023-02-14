822ef2ff89f486ae4d9e9e2d7ff8974d144c8980
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2023-02-14 16:50:00 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,17 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyDE0UIiDBi02bpq2ISa4zS0fghzG-of1ow",
+  authDomain: "projeto67-65243.firebaseapp.com",
+  projectId: "projeto67-65243",
+  storageBucket: "projeto67-65243.appspot.com",
+  messagingSenderId: "993003702139",
+  appId: "1:993003702139:web:6201e84f277fa7944f5fde"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
